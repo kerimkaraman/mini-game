d4512eb4a03def470b564237c2881938b924fc5b
Kerim Karaman <55785875+kerimkaraman@users.noreply.github.com>
2023-09-14 17:18:41 +0300

    ...

--- screens/GameScreen.jsx
+++ screens/GameScreen.jsx
@@ -5,6 +5,7 @@ import PrimaryButton from '../components/ui/PrimaryButton';
 import NumberContainer from '../components/game/NumberContainer';
 import Card from '../components/ui/Card';
 import InstructionText from '../components/ui/InstructionText';
+import { Ionicons } from '@expo/vector-icons'
 
 function generateRandomBetween(min, max, exclude) {
     const rndNum = Math.floor(Math.random() * (max - min)) + min;
@@ -68,10 +69,14 @@ export default function GameScreen({ userNumber, onGameOver }) {
                 <InstructionText>Higher or Lower?</InstructionText>
                 <View style={styles.buttonsContainer}>
                     <View style={styles.buttonContainer}>
-                        <PrimaryButton onPress={nextGuessHandler.bind(this, "lower")}>-</PrimaryButton>
+                        <PrimaryButton onPress={nextGuessHandler.bind(this, "lower")}>
+                            <Ionicons name="md-remove" size={24} color="white" />
+                        </PrimaryButton>
                     </View>
                     <View style={styles.buttonContainer}>
-                        <PrimaryButton onPress={nextGuessHandler.bind(this, "greater")}>+</PrimaryButton>
+                        <PrimaryButton onPress={nextGuessHandler.bind(this, "greater")}>
+                            <Ionicons name="md-add" size={24} color="white" />
+                        </PrimaryButton>
                     </View>
                 </View>
             </Card>
